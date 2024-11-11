a0544572c07a2972c155a8185665f10c2b0daa46
KimHuiseon <envy089@gmail,com>
2024-11-11 16:32:05 +0900

    #8 [MOD] : Updated navHostFragment with bottomNavigationView setup

--- app/src/main/java/com/piooda/recycrew/feature/MainActivity.kt
+++ app/src/main/java/com/piooda/recycrew/feature/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.piooda.recycrew.R
 import com.piooda.recycrew.databinding.ActivityMainBinding
-import com.piooda.recycrew.feature.community.CommunityFragment
-import com.piooda.recycrew.feature.event.EventFragment
-import com.piooda.recycrew.feature.home.HomeFragment
-import com.piooda.recycrew.feature.myPage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -24,47 +21,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
         navController = navHostFragment.navController
-        setBottomNavigationView()
-
-        // 앱 초기 실행 시 홈화면으로 설정
-        if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_home
-        }
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
-
-    fun setBottomNavigationView() {
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.fragment_home -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.fcv_main, HomeFragment())
-                        .commit()
-                    true
-                }
-
-                R.id.fragment_community -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(
-                            R.id.fcv_main,
-                            CommunityFragment()
-                        ).commit()
-                    true
-                }
-
-                R.id.fragment_event -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fcv_main, EventFragment()).commit()
-                    true
-                }
-
-                R.id.fragment_mypag -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.fcv_main, MyPageFragment()).commit()
-                    true
-                }
-
-                else -> false
-            }
-        }
-    }
-
 }
